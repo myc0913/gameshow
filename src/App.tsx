867736ad1d88ac7ff7966cfbc78429f63304bd30
myc0913 <248091018@qq.com>
2026-06-12 01:10:46 +0800

    A0: add App shell with tab switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+// src/App.tsx
+
+import { useState } from 'react';
+import { TabNav, type TabId } from './components/TabNav';
 import { PlayPage } from './pages/PlayPage';
+import { HowPage } from './pages/HowPage';
+import { WhyPage } from './pages/WhyPage';
+import './styles.css';
 
 function App() {
-  return <PlayPage />;
+  const [activeTab, setActiveTab] = useState<TabId>('play');
+
+  const renderPage = () => {
+    switch (activeTab) {
+      case 'play':
+        return <PlayPage />;
+      case 'how':
+        return <HowPage />;
+      case 'why':
+        return <WhyPage />;
+    }
+  };
+
+  return (
+    <div className="app-shell">
+      <TabNav activeTab={activeTab} onTabChange={setActiveTab} />
+      {renderPage()}
+    </div>
+  );
 }
 
 export default App;
